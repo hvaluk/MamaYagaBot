252e05d533ed38bb71c6d6ee44b379e5fbdae7e9
Aliaksandra Hvaluk <a.hvaluk@gmail.com>
2025-11-24 13:15:29 +0100

    create user model

---
+++ src/handlers/dao/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import UTC, datetime
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'MamaYogaBot_users'
+
+    telegram_id = Column(Integer, primary_key=True) # Telegram user ID
+    username = Column(String, nullable=True) # Telegram username (can be null   if not set )
+    first_name = Column(String, nullable=False) # User's first name
+    last_name = Column(String, nullable=True) # User's last name (can be null if not set)
+    phone = Column(String(20), nullable=True) # User's phone number (if provided)
+    is_admin = Column(Boolean, default=False) # Is the user an admin
+    is_active = Column(Boolean, default=True) # Is the user active
+    registered_at = Column(DateTime, default=datetime.now(tz=UTC)) # Registration timestamp      
+
+    def __repr__(self):
+        return f"<User(id={self.telegram_id}, username='{self.username}')>"
